2613e0059535cf1ea0f89d1f4eac23a036feae02
william.farre@gmail.com <william.farre@gmail.com>
2022-08-04 23:04:17 +0800

    customize tooltip cursor

--- src/components/Charts/CurveChart.jsx
+++ src/components/Charts/CurveChart.jsx
@@ -5,7 +5,7 @@ import {
   XAxis,
   Tooltip,
   ResponsiveContainer,
-  ReferenceArea,
+  Rectangle,
 } from "recharts";
 import { useFetch } from "../../utils/hooks";
 
@@ -29,6 +29,7 @@ function CurveChart(props) {
    */
   function CustomTooltip({ payload, label, active }) {
     if (active) {
+      console.log(payload);
       return (
         <div
           className="custom-tooltip"
@@ -40,7 +41,6 @@ function CurveChart(props) {
             flexDirection: "column",
             justifyContent: "space-around",
             alignItems: "center",
-            // boxShadow: "20px 20px white",
           }}
         >
           <p
@@ -60,22 +60,21 @@ function CurveChart(props) {
     return null;
   }
 
-  function customActiveDot(cx, cy, value) {
+  const CustomCursor = (props) => {
+    const { points } = props;
+    const { x, y } = points[0];
     return (
-      <circle
-        // cx={160}
-        // cy={50}
-        r={25}
-        stroke="black"
-        strokeWidth={3}
-        fill="white"
+      <Rectangle
+        fill="#000000"
+        // stroke="red"
+        x={x}
+        y={y}
+        width={500}
+        height={500}
+        opacity={0.1}
       />
-      // <div
-      //   className="dot"
-      //   style={{ height: "20px", width: "20px", backgroundColor: "blue" }}
-      // ></div>
     );
-  }
+  };
 
   //we set the day with the highest activity
   const off = Math.floor((data.maxDay / 7) * 100);
@@ -98,12 +97,6 @@ function CurveChart(props) {
             data={data.sessions}
             style={{
               background: "#FF0000",
-              // background:
-              //   "linear-gradient(to right, #FF0000, #FF0000 " +
-              //   off +
-              //   "% , rgb(211,45,31) " +
-              //   off +
-              //   "%, rgb(211,45,31))",
             }}
           >
             <defs>
@@ -123,24 +116,8 @@ function CurveChart(props) {
 
             <Tooltip
               content={CustomTooltip}
-              // allowEscapeViewBox={{
-              //   x: true,
-              //   y: true,
-              // }}
-              // offset={20}
-              // cursor={{
-              //   stroke: "black",
-              //   // stroke: "400px",
-              //   strokeWidth: "50%",
-              //   strokeOpacity: 0.3,
-              //   style: {
-              //     position: "absolute",
-              //     height: "200%",
-              //     left: "50%",
-              //   },
-              // }}
               active={false}
-              cursor={false}
+              cursor={<CustomCursor />}
               tick={{
                 fill: "#FFF",
                 fontFamily: "Roboto",
@@ -170,11 +147,8 @@ function CurveChart(props) {
                 strokeWidth: 4,
                 r: 3,
                 fill: "white",
-                style: { boxShadow: "16px 16px black" },
               }}
-              // activeDot={customActiveDot}
             />
-            {/* <ReferenceArea x1={data.maxDay} x2={7} /> */}
           </AreaChart>
         </ResponsiveContainer>
       )}
